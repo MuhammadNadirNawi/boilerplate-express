889c7f61ef2d1afd61f175f3f64559ed664ab48f
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-01-03 19:41:23 +0800

    add route now

--- myApp.js
+++ myApp.js
@@ -34,6 +34,13 @@ app.get('/json',function(req, res, next){
   next()
 },
 )
+app.get('/now',function(req, res, next){
+  req.time = new Date().toString()
+  next()
+},function (req, res) {
+  res.json({time: req.time})
+}
+)
 
 
 
