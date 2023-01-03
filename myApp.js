ace61ed800ba99f520ad41cd14cbffa93d325e02
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-01-03 16:23:54 +0800

    add codition env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
@@ -11,11 +12,20 @@ app.use('/public', express.static(__dirname + "/public"))
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/views/index.html")
 })
-app.get('/json', function(req, res){
-  res.json({
-    "message": "Hello json"
-  })
-})
+
+app.get('/json',function(req, res){
+
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json(
+      {"message": "HELLO JSON"}
+    )
+  }else{
+    res.json(
+      {"message": "Hello json"}
+    )
+  }
+},
+)
 
 
 
